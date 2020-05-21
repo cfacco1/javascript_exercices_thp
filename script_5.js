5675a653dd19b573d53b825449fb4e0f0654d883
cfacco1 <facco.clement@gmail.com>
2020-05-21 21:49:00 +0200

    Exo 5

--- script_5.js
+++ script_5.js
@@ -14,8 +14,46 @@ const books = [
 ];
 
 //Est-ce que tous les livres ont été au moins empruntés une fois ?
+
+console.log('Est-ce que tous les livres ont été au moins empruntés une fois ?');
+for(let index in books) {;
+  if (books[index].rented<= 1) {;
+    console.log ("Non, un livre n'a pas été emprunté");
+    } else {
+      console.log ("tous les livres ont été empruntés au moins une fois");
+    };
+  };
+
 // Quel est livre le plus emprunté ?
+
+let booksRentedBest = books.sort(function (a, b) {;
+  return b.rented - a.rented;
+});
+console.log("Quel est livre le moins emprunté ?");
+console.log(booksRentedBest[0].title);
+
 // Quel est le livre le moins emprunté ?
+
+let booksRentedWorst = books.sort(function (a, b) {;
+  return a.rented - b.rented;
+});
+console.log("Quel est livre le moins emprunté ?");
+console.log(booksRentedWorst[0].title);
+
 // Trouve le livre avec l'ID: 873495 ;
+
+let BookFind = books.find(item => item.id == 873495);
+console.log("Le livre avec l'ID: 873495 est :" + BookFind.title);
+
 // Supprime le livre avec l'ID: 133712 ;
+
+console.log("Supprime le livre avec l'ID: 133712" )
+let bookDelete = books.findIndex(item => item.id === 133712);
+books.splice(bookDelete,1);
+console.log(books);
+
 // Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé).
+
+console.log("Trie des livres par ordre alphabétique")
+books.sort((a, b) => (a.title > b.title) ? 1 : -1);
+console.log(books);
